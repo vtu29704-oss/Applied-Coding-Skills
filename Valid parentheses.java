acff7613d44dd2296b70b9b45547d38f24a34aaf
Y Bhavya Sri <vtu29704@veltech.edu.in>
2026-09-11 18:30:09 +0530

    Add files via upload

---
+++ Valid parentheses.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+Case 1:      Case 2:
+
+Input:       Input:
+s = "()"     s = "([)]"
+
+Output:      Output:
+true         false
+
+Expected:    Expected:
+true         false
